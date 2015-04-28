9a067354b21b8b3a4f555380b8cd599c5deeb5b5
Paul Torka <paul.torka@peerigon.com>
2015-04-28 09:40:37 +0200

    - Added atc.xlsx to fixtures - Increased timeout for integration tests - Compares now shasum of downloaded with reference - server exposes now it's config

--- test_integration/job/swissmedix.test.js
+++ test_integration/job/swissmedix.test.js
@@ -1,37 +1,53 @@
 "use strict";
 
 var rewire = require("rewire");
+var shasum = require("shasum");
+var xlsx = require("xlsx");
+var expect = require("chai").expect;
+
 var server = require("../server");
 
 describe("job: siwssmedic", function () {
-  var job, config;
+  var job, cfg, test;
 
-  before(function (done) {
-    config = require("../../config.json");
-
-    job = rewire("../../jobs/swissmedic");
-    job.__set__("cfg", {
-      download: {
-        url: "http://localhost:" + server.port + "/arzneimittel/00156/00221/00222/00230/index.html",
-        dir: "./test_integration/tmp/data/auto",
-        file: "./test_integration/tmp/data/auto/swissmedic.xlsx"
-      },
-      process: {
-        dir: "./test_integration/tmp/data/release/swissmedic/",
-        file: "./test_integration/tmp/data/release/swissmedic/swissmedic.json",
-        minFile: "./test_integration/tmp/data/release/swissmedic/siwssmedic.min.json"
+  // start server
+  before(server.spinUp);
+  // create test config
+  before(function () {
+    test = {
+      cfg: {
+        download: {
+          url: "http://localhost:" + server.port + "/arzneimittel/00156/00221/00222/00230/index.html",
+          dir: "./test_integration/tmp/data/auto",
+          file: "./test_integration/tmp/data/auto/swissmedic.xlsx"
+        },
+        process: {
+          dir: "./test_integration/tmp/data/release/swissmedic/",
+          file: "./test_integration/tmp/data/release/swissmedic/swissmedic.json",
+          minFile: "./test_integration/tmp/data/release/swissmedic/siwssmedic.min.json"
+        }
       }
-    });
+    };
+  });
+  // run job
+  before(function (done) {
+    job = rewire("../../jobs/swissmedic");
+    job.__set__("cfg", test.cfg);
 
-    server.spinUp(done);
+    job(done);
   });
 
-  it("should download and process xlsx-file properly", function (done) {
-    job(function () {
-      //@TODO assertions, e.g. file size and
-      done();
+  describe("XLSX-Download", function () {
+    // very slow: ~20000ms
+    it("should download whole xlsx-File from swissmedic", function () {
+      expect(shasum(xlsx.readFile(server.cfg.swissmedic.xlsx))).to.equal(shasum(xlsx.readFile(test.cfg.download.file)));
     });
   });
 
+  // stop server
   after(server.spinDown);
+  // clean after job
+  after(function (done) {
+    done();
+  });
 });
--- test_integration/server.js
+++ test_integration/server.js
@@ -11,24 +11,41 @@ var config = require("../config.json");
 var app = express();
 var server;
 var port = 3001;
-var swissmedicDownloadPath = url.parse(config.swissmedic.download.url).path;
-var qs = { download: "NHzLpZeg7t,lnp6I0NTU042l2Z6ln1acy4Zn4Z2qZpnO2Yuq2Z6gpJCDdHx7hGym162epYbg2c_JjKbNoKSn6A--"};
+var cfg = {
+  "swissmedic": {
+    "xlsx": path.resolve(__dirname, "./fixtures/swissmedic/download.xlsx"),
+    "html": path.resolve(__dirname, "./fixtures/swissmedic/index.html"),
+    "path": url.parse(config.swissmedic.download.url).path,
+    "qs": { download: "NHzLpZeg7t,lnp6I0NTU042l2Z6ln1acy4Zn4Z2qZpnO2Yuq2Z6gpJCDdHx7hGym162epYbg2c_JjKbNoKSn6A--" }
+  }
+};
 
-app.get(swissmedicDownloadPath, function (req, res) {
+app.get(cfg.swissmedic.path, function (req, res) {
   // simulate download
-  if (req.query.download === qs.download) {
-    res.sendFile(path.resolve(__dirname, "./fixtures/swissmedic/download.xlsx"));
+  if (req.query.download === cfg.swissmedic.qs.download) {
+    res.sendFile(cfg.swissmedic.xlsx);
     return;
   }
 
   // serve page
-  fs.createReadStream(path.resolve(__dirname, "./fixtures/swissmedic/index.html")).pipe(res);
+  fs.createReadStream(cfg.swissmedic.html).pipe(res);
 });
 
+exports.cfg = cfg;
 exports.port = port;
+
+/**
+ *
+ * @param {function(Error|null)} done
+ */
 exports.spinUp = function (done) {
   server = app.listen(port, done);
 };
+
+/**
+ *
+ * @param {function(Error|null)} done
+ */
 exports.spinDown = function (done) {
   server.close(done);
 };
