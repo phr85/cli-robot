9781b83abffaa6d04078631cb5d9673aa11387e6
Paul Torka <paul.torka@peerigon.com>
2015-04-29 16:38:46 +0200

    Fixed parseLinks so that it will also work with atc-Job.

--- test/lib/parseLink.test.js
+++ test/lib/parseLink.test.js
@@ -10,20 +10,42 @@ describe("parseLinks", function () {
 
   before(function () {
     parseLink = require("../../lib/parseLink");
-    url = "https://www.swissmedic.ch/arzneimittel/00156/00221/00222/00230/index.html"
-    html = fs.readFileSync(path.resolve(__dirname, "../fixtures/www.swissmedic.ch.html"), {encoding: "utf8"});
-    ref = url + "?lang=de&download=NHzLpZeg7t,lnp6I0NTU042l2Z6ln1acy4Zn4Z2qZpnO2Yuq2Z6gpJCDdHx7hGym162epYbg2c_JjKbNoKSn6A--";
   });
 
-  beforeEach(function () {
-    regex = /href="([\/a-zäöü0-9\?\;\,\=\.\-\_\&]*)".*Excel-Version Zugelassene Verpackungen/i;
+  describe("swissmedic", function () {
+    before(function () {
+      url = "https://www.swissmedic.ch/arzneimittel/00156/00221/00222/00230/index.html";
+      html = fs.readFileSync(path.resolve(__dirname, "../fixtures/www.swissmedic.ch.html"), {encoding: "utf8"});
+      ref = url + "?lang=de&download=NHzLpZeg7t,lnp6I0NTU042l2Z6ln1acy4Zn4Z2qZpnO2Yuq2Z6gpJCDdHx7hGym162epYbg2c_JjKbNoKSn6A--";
+    });
+
+    beforeEach(function () {
+      regex = /href="([\/a-zäöü0-9\?\;\,\=\.\-\_\&]*)".*Excel-Version Zugelassene Verpackungen/ig;
+    });
+
+    it("should parse link out of given html correctly", function () {
+      expect(parseLink(url, html, regex)).to.equal(ref);
+    });
+
+    // @TODO move to a more generic test
+    it("should replace '&amp;' with '&'", function () {
+      expect(parseLink(url, html, regex)).to.not.contain("&amp;");
+    });
   });
 
-  it("should parse link out of given html correctly", function () {
-    expect(parseLink(url, html, regex)).to.equal(ref);
-  });
+  describe("atc", function () {
+    before(function () {
+      url = "http://www.wido.de/amtl_atc-code.html";
+      html = fs.readFileSync(path.resolve(__dirname, "../fixtures/amtl_atc-code.html"), {encoding: "utf8"});
+      ref = "http://www.wido.de/fileadmin/wido/downloads/pdf_arzneimittel/atc/wido_arz_amtl_atc-index_1214.zip";
+    });
 
-  it("should replace '&amp;' with '&'", function () {
-    expect(parseLink(url, html, regex)).to.not.contain("&amp;");
+    beforeEach(function () {
+      regex = /href="(.*atc.*\.zip)"/img;
+    });
+
+    it("should parse link out of given html correctly", function () {
+      expect(parseLink(url, html, regex)).to.equal(ref);
+    });
   });
 });
